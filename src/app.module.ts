527de4d9c66c6faf3f60a80099adc9c1c6a4e9d1
alisonratcliffe93@gmail.com <68648399+ratcliffealison@users.noreply.github.com>
2025-06-17 10:03:03 +0200

    Install mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, CommentsModule],
+  imports: [UsersModule, CommentsModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
